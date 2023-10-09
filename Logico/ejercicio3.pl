b453787fb95b49d71fb97dc9246f4e2c7cde7a98
yugiiGX <yugiiGX@outlook.es>
2023-10-09 20:44:51 -0300

    logico 8-12

--- Logico/ejercicio3.pl
+++ Logico/ejercicio3.pl
@@ -31,3 +31,26 @@ palindromo(L1) :- invertir_lista(L1,L2) , L1 = L2.
 % de la lista L.
 ultimo(X,[X]).
 ultimo(X,[_|Resto]) :- ultimo(X,Resto),!.
+
+%8- Utilizando el predicado select, definir la relación inserta(X,L1,L2) que verifique si L2 es una
+% lista obtenida insertando X en L1. Compruebe el resultado de la consulta inserta(a,[1,2],L).
+
+inserta(X, L1, L2) :- select(X, L2, L1).
+
+%9- Utilizando el predicado append, definir la relación sublista(L1,L2) que verifique si L1 es
+% una sublista de L2.
+
+sublista(L1,L2) :- append(_,Sublista,L2) , append(L1,_,Sublista).
+
+%10- Definir la relación subconjunto(L1,L2) que verifique si L2 es un subconjunto de L1.
+subconjunto([],_).
+subconjunto([X|L1],L2) :- member(X,L2),subconjunto(L1,L2).
+
+%11- Definir la relación maximo(X,Y,Z) (equivalente a max) que verifique si Z es el maximo de
+% X e Y
+maximo(X,Y,Z) :- X > Y , Z = X ,!.
+maximo(X,Y,Z) :- Z = Y.
+
+%12-  Definir la relación mcd(X,Y,Z) que verifique si Z es el maximo común divisor de X e Y.
+mcd(X,0,X) :- X > 0.
+mcd(X,Y,Z) :- Y > 0, Resto is X mod Y , mcd(Y,Resto,Z).
