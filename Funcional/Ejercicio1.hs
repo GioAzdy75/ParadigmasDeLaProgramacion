d64d051848af255b40d2ce0df8b6dd022f68f19d
yugiiGX <yugiiGX@outlook.es>
2023-10-12 22:53:33 -0300

    Haciendo Tp Funcional

--- Funcional/Ejercicio1.hs
+++ Funcional/Ejercicio1.hs
@@ -6,3 +6,52 @@ volumenEsfera r = 4/3 * pi * r^3
 -- monedas de 1 centavo, b de 5 centavos, c de 10 centavos, d de 50 centavos, e de 1 peso.
 sumaCoins :: (Int, Int, Int, Int, Int) -> Float
 sumaCoins (a, b, c, d, e) = fromIntegral a * 0.01 + fromIntegral b * 0.05 + fromIntegral c * 0.1  + fromIntegral d * 0.5  + fromIntegral e
+
+-- C. Incrementar todos los elementos de una tupla de tres enteros.
+incrementarTuplas :: (Int,Int,Int) -> (Int,Int,Int)
+incrementarTuplas (a,b,c) = (a+1,b+1,c+1)
+
+-- D. Calcular el cuadrado de un número.
+cuadradoNumero :: Float -> Float
+cuadradoNumero x = x * x
+
+-- E. calcular el valor de un número elevado a la 4 utilizando la función del inciso anterior
+elevado_4 :: Float -> Float
+elevado_4 x = cuadradoNumero(cuadradoNumero x)
+
+-- F. Calcular la media aritmética de tres valores numéricos.
+mediaAritmetica :: Float -> Float -> Float -> Float
+mediaAritmetica a b c = (a + b + c) / 3 
+
+-- G. Calcular el máximo entre 3 números, puede utilizar la función predefinida max
+maximo3Numeros :: Float -> Float -> Float -> Float
+maximo3Numeros a b c = max (max a b) c
+
+-- H. Calcular el máximo entre 6 números, puede utilizar la función predefinida max
+maximo6Numeros :: Float -> Float -> Float -> Float -> Float -> Float -> Float
+maximo6Numeros a b c d e f = max (maximo3Numeros a b c) (maximo3Numeros d e f) 
+
+-- I. Definir la función area tal que la función área a b c es el área de un triangulo de lados a,b,c.
+--areaTriangulo :: Float -> Float -> Float -> Float
+--areaTriangulo a b _ = a * b / 2
+
+-- J. Definir una función tal que reciba un par (x, y) y retorne el cuadrante, puede informar mediante
+--mensaje o devolviendo el número de cuadrante. El par no esta sobre los ejes x ni y.
+
+-- K. La función igualesT res verifica que tres elementos x, y y z son iguales.
+igualesTres :: Eq a => a -> a -> a -> Bool
+igualesTres a b c = a == b && b == c
+-- igualesTres a b c = (a == b) == c # Porque Error? :Rta (a==b) devuelve un Bool y luego con el otro == estamos comparando un bool con un Num y da error
+
+
+
+
+-- G. Definir la función nIndex tal que nIndex l n es elemento enésimo de l, empezando a numerar con el 0.
+--nIndex [] _ = error "Lista Vacia"
+nIndex :: [a] -> Int -> a
+nIndex l n  | l == [] = error "Lista Vacia"
+            | n < 0 = error "Indice Fuera de Rango"
+            | n >= length l = error "Indice Fuera de Rango" 
+            
+nIndex (x:_) 0 = x
+nIndex (_:xs) n = nIndex xs (n-1)
